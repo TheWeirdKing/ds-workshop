615efdb8d5296d84d717bc217ead46b5eca1c8fd
NicholasCiuica <nick.ciuica@gmail.com>
2024-10-04 14:56:33 -0400

    Fixed Silly Warning

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]){
 
-  for(unsigned int i = 1; i < argc; i++){
+  for(int i = 1; i < argc; i++){
     for(int j = 0; j < 2; j++) { 
       std::cout << argv[i] << std::endl;
     }
